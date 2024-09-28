8a7ba1b62a81c04157076e38048012ff9e7157f9
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2024-09-28 15:43:09 +0630

    feat: bookmark e2e test

---
+++ src/bookmark/bookmark.service.ts
@@ -0,0 +1,79 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class BookmarkService {
+  constructor(private prisma: PrismaService) {}
+
+  async getBookmarks(userId: number) {
+    return await this.prisma.bookmark.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async getBookmarkById(userId: number, bookmarkId: number) {
+    return await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+        userId,
+      },
+    });
+  }
+
+  async createBookmark(userId: number, dto: CreateBookmarkDto) {
+    const bookmark = await this.prisma.bookmark.create({
+      data: {
+        ...dto,
+        userId,
+      },
+    });
+
+    return bookmark;
+  }
+
+  async editBookmarkById(
+    userId: number,
+    bookmarkId: number,
+    dto: EditBookmarkDto,
+  ) {
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark || bookmark.userId !== userId) {
+      throw new NotFoundException('Bookmark does not exist');
+    }
+
+    return await this.prisma.bookmark.update({
+      where: {
+        id: bookmarkId,
+      },
+      data: {
+        ...dto,
+      },
+    });
+  }
+
+  async deleteBookmarkById(userId: number, bookmarkId: number) {
+    const bookmark = await this.prisma.bookmark.findUnique({
+      where: {
+        id: bookmarkId,
+      },
+    });
+
+    if (!bookmark || bookmark.userId !== userId) {
+      throw new NotFoundException('Bookmark does not exist');
+    }
+
+    return await this.prisma.bookmark.delete({
+      where: {
+        id: bookmarkId,
+      },
+    });
+  }
+}
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -5,6 +5,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 import { AuthDto } from 'src/auth/dto';
 import { EditUserDto } from 'src/user/dto/edit-user.dto';
+import { CreateBookmarkDto, EditBookmarkDto } from 'src/bookmark/dto';
 
 describe('App E2E', () => {
   let app: INestApplication;
@@ -102,7 +103,8 @@ describe('App E2E', () => {
           .post('/auth/signin')
           .withBody(dto)
           .expectStatus(200)
-          .stores('userAccessToken', 'access_token');
+          .stores('userAccessToken', 'access_token')
+          .stores('userId', 'id');
       });
     });
   });
@@ -123,7 +125,7 @@ describe('App E2E', () => {
     });
 
     describe('Edit user', () => {
-      it('Should edit user', () => {
+      it('Should edit current user', () => {
         return spec()
           .patch('/users')
           .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
@@ -131,6 +133,72 @@ describe('App E2E', () => {
           .expectStatus(200)
           .expectBodyContains(userDto.email)
           .expectBodyContains(userDto.firstName);
+      });
+    });
+  });
+
+  describe('Bookmark Module', () => {
+    describe('Create bookmark', () => {
+      const bookmarkDto: CreateBookmarkDto = {
+        title: 'The Fall',
+        link: 'https://en.wikipedia.org/wiki/The_Fall_(Camus_novel)',
+        description: 'The Fall by Albert Camus',
+      };
+      it('Should create bookmark', () => {
+        return spec()
+          .post('/bookmarks')
+          .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
+          .withBody({ ...bookmarkDto, userId: '$S{userId}' })
+          .expectStatus(201)
+          .stores('bookmarkId', 'id')
+          .expectBodyContains(bookmarkDto.title)
+          .expectBodyContains(bookmarkDto.link)
+          .expectBodyContains(bookmarkDto.description);
+      });
+    });
+
+    describe('Get bookmarks', () => {
+      it('Should get bookmarks', () => {
+        return spec()
+          .get('/bookmarks')
+          .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
+          .expectStatus(200)
+          .expectJsonLength(1);
+      });
+    });
+
+    describe('Get bookmark By Id', () => {
+      it('Should get bookmark by id', () => {
+        return spec()
+          .get('/bookmarks/{id}')
+          .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
+          .withPathParams({ id: '$S{bookmarkId}' })
+          .expectStatus(200);
+      });
+    });
+
+    describe('Edit bookmark by Id', () => {
+      const bookmarkDto: EditBookmarkDto = {
+        title: 'The Fall (Albert Camus)',
+      };
+      it('Should edit bookmark', () => {
+        return spec()
+          .patch('/bookmarks/{id}')
+          .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
+          .withPathParams({ id: '$S{bookmarkId}' })
+          .withBody({ ...bookmarkDto, userId: '$S{userId}' })
+          .expectStatus(200)
+          .expectBodyContains(bookmarkDto.title);
+      });
+    });
+
+    describe('Delete bookmark by Id', () => {
+      it('Should delete bookmark', () => {
+        return spec()
+          .delete('/bookmarks/{id}')
+          .withHeaders('Authorization', 'Bearer $S{userAccessToken}')
+          .withPathParams({ id: '$S{bookmarkId}' })
+          .expectStatus(204);
       });
     });
   });
