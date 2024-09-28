86942784c9aa478bfddc7ff8c1a75cc1f8e18b5b
Kyaw Zin Htet <itskyawzinhtet@gmail.com>
2024-09-28 13:18:08 +0630

    feat: auth e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,9 @@
 import { Test } from '@nestjs/testing';
+import { request, spec } from 'pactum';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
+import { AuthDto } from 'src/auth/dto';
 
 describe('App E2E', () => {
   let app: INestApplication;
@@ -21,9 +23,12 @@ describe('App E2E', () => {
     );
 
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDB();
+
+    request.setBaseUrl('http://localhost:3333');
   });
 
   afterAll(async () => {
@@ -31,12 +36,73 @@ describe('App E2E', () => {
   });
 
   describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'test@test.com',
+      password: 'p@ssword',
+    };
+
     describe('Signup', () => {
-      it.todo('should signup');
+      it('Should not Signup without email', () => {
+        return spec()
+          .post('/auth/signup')
+          .withBody(dto.password)
+          .expectStatus(400);
+      });
+    });
+
+    describe('Signup', () => {
+      it('Should not Signup without password', () => {
+        return spec()
+          .post('/auth/signup')
+          .withBody(dto.email)
+          .expectStatus(400);
+      });
+    });
+
+    describe('Signup', () => {
+      it('Should not Signup without body', () => {
+        return spec().post('/auth/signup').expectStatus(400);
+      });
+    });
+
+    describe('Signup', () => {
+      it('Should Signup', () => {
+        return spec().post('/auth/signup').withBody(dto).expectStatus(201);
+      });
     });
 
     describe('Signin', () => {
-      it.todo('should signin');
+      it('Should not Signin without email', () => {
+        return spec()
+          .post('/auth/signin')
+          .withBody(dto.password)
+          .expectStatus(400);
+      });
+    });
+
+    describe('Signin', () => {
+      it('Should not Signin without password', () => {
+        return spec()
+          .post('/auth/signin')
+          .withBody(dto.email)
+          .expectStatus(400);
+      });
+    });
+
+    describe('Signin', () => {
+      it('Should not Signin without body', () => {
+        return spec().post('/auth/signin').expectStatus(400);
+      });
+    });
+
+    describe('Signin', () => {
+      it('Should Signin', () => {
+        return spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAccessToken', 'access_token');
+      });
     });
   });
 
